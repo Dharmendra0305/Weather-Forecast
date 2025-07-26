dd1e2a9f3909d1da65c4797328d6b54f5ad900ab
Dharmendra0305 <dharmendra.kp03@gmail.com>
2025-07-26 18:02:12 +0530

    update WeatherResponse.java file

--- src/main/java/org/example/model/WeatherResponse.java
+++ src/main/java/org/example/model/WeatherResponse.java
@@ -1,7 +1,12 @@
 package org.example.model;
 
+import lombok.Getter;
+import lombok.Setter;
+
 import java.util.List;
 
+@Setter
+@Getter
 public class WeatherResponse
 {
     private String name;
@@ -10,135 +15,33 @@ public class WeatherResponse
     private Main main;
     private Wind wind;
 
-    public String getName()
-    {
-        return name;
-    }
-
-    public void setName(String name)
-    {
-        this.name = name;
-    }
-
-    public Sys getSys()
-    {
-        return sys;
-    }
-
-    public void setSys(Sys sys)
-    {
-        this.sys = sys;
-    }
-
-    public List<Weather> getWeather()
-    {
-        return weather;
-    }
-
-    public void setWeather(List<Weather> weather)
-    {
-        this.weather = weather;
-    }
-
-    public Main getMain()
-    {
-        return main;
-    }
-
-    public void setMain(Main main)
-    {
-        this.main = main;
-    }
-
-    public Wind getWind()
-    {
-        return wind;
-    }
-
-    public void setWind(Wind wind)
-    {
-        this.wind = wind;
-    }
-
+    @Getter @Setter
     public static class Sys
     {
         private String country;
 
-        public String getCountry()
-        {
-            return country;
-        }
-
-        public void setCountry(String country)
-        {
-            this.country = country;
-        }
     }
 
+    @Getter @Setter
     public static class Weather
     {
         private int id;
         private String description;
 
-        public int getId()
-        {
-            return id;
-        }
-
-        public void setId(int id)
-        {
-            this.id = id;
-        }
-
-        public String getDescription()
-        {
-            return description;
-        }
-
-        public void setDescription(String description)
-        {
-            this.description = description;
-        }
     }
 
+    @Getter @Setter
     public static class Main
     {
         private double temp;
         private int humidity;
 
-        public double getTemp()
-        {
-            return temp;
-        }
-
-        public void setTemp(double temp)
-        {
-            this.temp = temp;
-        }
-
-        public int getHumidity()
-        {
-            return humidity;
-        }
-
-        public void setHumidity(int humidity)
-        {
-            this.humidity = humidity;
-        }
     }
 
+    @Getter @Setter
     public static class Wind
     {
         private double speed;
 
-        public double getSpeed()
-        {
-            return speed;
-        }
-
-        public void setSpeed(double speed)
-        {
-            this.speed = speed;
-        }
     }
 }
